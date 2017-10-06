69c5436460564adaff5ab327b53e1f53a4b9e44d
Unknown <alexrclens59@gmail.com>
2017-10-06 16:55:59 +0200

    dicoTest

--- src/com/scrabble/model/Dictionnaire.java
+++ src/com/scrabble/model/Dictionnaire.java
@@ -1,9 +1,12 @@
 package com.scrabble.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by alexr on 06/10/2017.
  */
 public class Dictionnaire {
+    ArrayList<String> dico;
     public Dictionnaire(){
         
     }
