4f33460f2be2320119037afe0c3753c71c3e80a7
Unknown <alexrclens59@gmail.com>
2017-10-06 17:00:39 +0200

    dicoTest2

--- src/com/scrabble/model/Dictionnaire.java
+++ src/com/scrabble/model/Dictionnaire.java
@@ -1,6 +1,9 @@
 package com.scrabble.model;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by alexr on 06/10/2017.
@@ -8,6 +11,10 @@ import java.util.ArrayList;
 public class Dictionnaire {
     ArrayList<String> dico;
     public Dictionnaire(){
-        
+        try {
+            Scanner sc = new Scanner(new File("com/scrabble/ressources/dico_Scrabble.txt"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
