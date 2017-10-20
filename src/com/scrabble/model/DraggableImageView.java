bc08776a3e511a1942b848d62808a62d1a97250e
Typhon <loic.sculier@hotmail.fr>
2017-10-20 11:02:26 +0200

    DraggableImageView update

--- src/com/scrabble/model/DraggableImageView.java
+++ src/com/scrabble/model/DraggableImageView.java
@@ -1,7 +1,9 @@
 package com.scrabble.model;
 
+import javafx.scene.Cursor;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
 
 /**
  * Created by loic on 06/10/2017.
@@ -9,7 +11,8 @@ import javafx.scene.image.ImageView;
 public class DraggableImageView extends ImageView {
     private double mouseX ;
     private double mouseY ;
-
+    private double lastPosX ;
+    private double lastPosY ;
     public DraggableImageView()
     {
         super();
@@ -17,15 +20,39 @@ public class DraggableImageView extends ImageView {
         setOnMousePressed(event -> {
             mouseX = event.getSceneX() ;
             mouseY = event.getSceneY() ;
+            lastPosX = getLayoutX();
+            lastPosY=getLayoutY();
+            this.setCursor(Cursor.HAND);
         });
 
         setOnMouseDragged(event -> {
+
             double deltaX = event.getSceneX() - mouseX ;
             double deltaY = event.getSceneY() - mouseY ;
-            relocate(getLayoutX() + deltaX, getLayoutY() + deltaY);
-            mouseX = event.getSceneX() ;
-            mouseY = event.getSceneY() ;
+            /*System.out.println(deltaX + "Delta Y :" + deltaY);
+            System.out.println("Layout X  " + getLayoutX() + " Layout Y" + getLayoutY());
+            System.out.println("last Layout X  " + lastPosX + " last Layout Y" + lastPosY);*/
+
+                relocate(getLayoutX() + deltaX, getLayoutY() + deltaY);
+
+
+
+                mouseX = event.getSceneX();
+                mouseY = event.getSceneY();
+
         });
+
+
+
+        setOnMouseReleased(event -> {
+            if((getLayoutX() < 0 || getLayoutY() < 0) || (getLayoutX() > getScene().getHeight() || getLayoutY() > getScene().getWidth())) {
+
+                relocate(273, 468);
+            }
+        });
+
+
+
     }
     public DraggableImageView(Image image) {
         super(image);
