b8c2277ae53c7ea1b110afb380c9ad6a31a268cb
Typhon <loic.sculier@hotmail.fr>
2017-10-20 16:11:20 +0200

    add menu and slide in/out animation

--- src/com/scrabble/MainApp.java
+++ src/com/scrabble/MainApp.java
@@ -1,170 +1,160 @@
 package com.scrabble;
 
 import com.scrabble.controller.MainUIController;
-import com.scrabble.model.BonusCase;
 import com.scrabble.model.Case;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.VPos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import com.scrabble.model.Scrabble;
+import javafx.util.Duration;
 
-import java.awt.Button;
-import java.awt.List;
 import java.io.IOException;
-import java.util.Arrays;
-
-import static com.scrabble.model.BonusCase.MT;
 
 public class MainApp extends Application {
 
-	private Stage primaryStage;
-	private AnchorPane rootLayout;
-	private Scrabble scrabble = new Scrabble();;
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+    private Scrabble scrabble = new Scrabble();
+    ;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Scrabble JavaFX");
-		initRootLayout();
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Scrabble JavaFX");
+        initRootLayout();
+    }
 
-	/**
-	 * Initializes the root layout.
-	 */
-	public void initRootLayout() {
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("view/board.fxml"));
-			rootLayout = (AnchorPane) loader.load();
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/board.fxml"));
+            rootLayout = (AnchorPane) loader.load();
 
-			drawboard(rootLayout);
-			// Give the controller access to the main app.
-			MainUIController controller = loader.getController();
-			controller.setMainApp(this);
+            drawboard(rootLayout);
+            // Give the controller access to the main app.
+            MainUIController controller = loader.getController();
+            controller.setMainApp(this);
 
-			Scene scene = new Scene(rootLayout);
-			scene.getStylesheets().add(getClass().getResource("view/style.css").toExternalForm());
+            Scene scene = new Scene(rootLayout);
+            scene.getStylesheets().add(getClass().getResource("view/style.css").toExternalForm());
 
-			primaryStage.setHeight(625);
-			primaryStage.setWidth(725);
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-
-	public void drawboard(AnchorPane boardview) {
-		AnchorPane paneBoard = (AnchorPane) boardview.lookup("#board");
-		GridPane boardGrid = new GridPane();
-		// espace inter colonne et ligne
-		boardGrid.setVgap(2);
-		boardGrid.setHgap(2);
-		// pourcentage egale des cases (resizable)
-		int numColsRows = 15;
-		for (int i = 0; i < numColsRows; i++) {
-			ColumnConstraints colConst = new ColumnConstraints();
-			colConst.setPercentWidth(100.0 / numColsRows);
-			boardGrid.getColumnConstraints().add(colConst);
-		}
-		for (int i = 0; i < numColsRows; i++) {
-			RowConstraints rowConst = new RowConstraints();
-			rowConst.setPercentHeight(100.0 / numColsRows);
-			boardGrid.getRowConstraints().add(rowConst);
-		}
-		// ajout des cases (Pane)
-		for (int i = 0; i < numColsRows; i++) {
-			for (int j = 0; j < numColsRows; j++) {
-				StackPane p = new StackPane();
-				//p.setStyle("-fx-background-color:#126B40"); 
-				//p.getStyleClass().add("gradiantGeneral");
-				boardGrid.add(p, i, j);
-			}
-		}
-		//boardGrid.getStyleClass().add("gradiantGeneral");
-		setColor(boardGrid);
-		//boardGrid.setGridLinesVisible(true);
-		
-		boardGrid.setPadding(new Insets(5));
-		// fit parent
-		AnchorPane.setLeftAnchor(boardGrid, 0.0);
-		AnchorPane.setRightAnchor(boardGrid, 0.0);
-		AnchorPane.setTopAnchor(boardGrid, 0.0);
-		AnchorPane.setBottomAnchor(boardGrid, 0.0);
-
-		paneBoard.getChildren().add(boardGrid);
-	}
-	
-	private void setColor(GridPane board) {
-		Case[][] plateau = scrabble.getBoard();
-		StackPane sp = new StackPane();
-		for(int i = 0;i<225;i++){
-			switch(plateau[i/15][i%15].getBonus()) {
-				case MT:
-					sp = (StackPane) board.getChildren().get(i);
-					sp.getStyleClass().add("gradiantMT");
-					sp.getChildren().add(new Label("MT"));
-					break;
-				case MD:
-					sp = (StackPane) board.getChildren().get(i);
-					sp.getStyleClass().add("gradiantMD");
-					sp.getChildren().add(new Label("MD"));
-					break;
-				case LT:
-					sp = (StackPane) board.getChildren().get(i);
-					sp.getStyleClass().add("gradiantLT");
-					sp.getChildren().add(new Label("LT"));
-					break;
-				case LD:
-					sp = (StackPane) board.getChildren().get(i);
-					sp.getStyleClass().add("gradiantLD");
-					sp.getChildren().add(new Label("LD"));
-					break;
-				case Vide:
-					board.getChildren().get(i).getStyleClass().add("gradiantGeneral");
-					break;
-			}
-
-		}
-		//etoile
-		sp = (StackPane) board.getChildren().get(112);
-		sp.getChildren().clear();
-		sp.getStyleClass().add("gradiantMD");
-		sp.getChildren().add(new Label("\u2605"));
-	}
+            primaryStage.setHeight(625);
+            primaryStage.setWidth(725);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
-	
-	
-	/**
-	 * Returns the main stage.
-	 *
-	 * @return
-	 */
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
+    public void drawboard(AnchorPane boardview) {
+        AnchorPane paneBoard = (AnchorPane) boardview.lookup("#board");
+        GridPane boardGrid = new GridPane();
+        // espace inter colonne et ligne
+        boardGrid.setVgap(2);
+        boardGrid.setHgap(2);
+        // pourcentage egale des cases (resizable)
+        int numColsRows = 15;
+        for (int i = 0; i < numColsRows; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numColsRows);
+            boardGrid.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < numColsRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numColsRows);
+            boardGrid.getRowConstraints().add(rowConst);
+        }
+        // ajout des cases (Pane)
+        for (int i = 0; i < numColsRows; i++) {
+            for (int j = 0; j < numColsRows; j++) {
+                StackPane p = new StackPane();
+                //p.setStyle("-fx-background-color:#126B40");
+                //p.getStyleClass().add("gradiantGeneral");
+                boardGrid.add(p, i, j);
+            }
+        }
+        //boardGrid.getStyleClass().add("gradiantGeneral");
+        setColor(boardGrid);
+        //boardGrid.setGridLinesVisible(true);
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        boardGrid.setPadding(new Insets(5));
+        // fit parent
+        AnchorPane.setLeftAnchor(boardGrid, 0.0);
+        AnchorPane.setRightAnchor(boardGrid, 0.0);
+        AnchorPane.setTopAnchor(boardGrid, 0.0);
+        AnchorPane.setBottomAnchor(boardGrid, 0.0);
+
+        paneBoard.getChildren().add(boardGrid);
+    }
+
+    private void setColor(GridPane board) {
+        Case[][] plateau = scrabble.getBoard();
+        StackPane sp = new StackPane();
+        for (int i = 0; i < 225; i++) {
+            switch (plateau[i / 15][i % 15].getBonus()) {
+                case MT:
+                    sp = (StackPane) board.getChildren().get(i);
+                    sp.getStyleClass().add("gradiantMT");
+                    sp.getChildren().add(new Label("MT"));
+                    break;
+                case MD:
+                    sp = (StackPane) board.getChildren().get(i);
+                    sp.getStyleClass().add("gradiantMD");
+                    sp.getChildren().add(new Label("MD"));
+                    break;
+                case LT:
+                    sp = (StackPane) board.getChildren().get(i);
+                    sp.getStyleClass().add("gradiantLT");
+                    sp.getChildren().add(new Label("LT"));
+                    break;
+                case LD:
+                    sp = (StackPane) board.getChildren().get(i);
+                    sp.getStyleClass().add("gradiantLD");
+                    sp.getChildren().add(new Label("LD"));
+                    break;
+                case Vide:
+                    board.getChildren().get(i).getStyleClass().add("gradiantGeneral");
+                    break;
+            }
+
+        }
+        //etoile
+        sp = (StackPane) board.getChildren().get(112);
+        sp.getChildren().clear();
+        sp.getStyleClass().add("gradiantMD");
+        sp.getChildren().add(new Label("\u2605"));
+    }
+
+
+
+
+    /**
+     * Returns the main stage.
+     *
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/com/scrabble/controller/MainUIController.java
+++ src/com/scrabble/controller/MainUIController.java
@@ -1,16 +1,35 @@
 package com.scrabble.controller;
 
 import com.scrabble.MainApp;
+import com.scrabble.model.ImageButton;
+import javafx.animation.PathTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
 
 public class MainUIController {
+
 
     public MainUIController() {
 
     }
 
+    @FXML
+    AnchorPane menu;
     public MainApp mainApp;
 
     @FXML
@@ -18,6 +37,9 @@ public class MainUIController {
 
     @FXML
     private Pane logoContainer;
+
+    @FXML
+    private ImageButton MenuBtn;
 
 
     /**
@@ -35,8 +57,39 @@ public class MainUIController {
      * after the fxml file has been loaded.
      */
     @FXML
-    private void initialize() {
-        logo.fitWidthProperty().bind(logoContainer.widthProperty());
+    private void initialize() throws IOException {
+       /*  Pane menuContent;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/Menu.fxml"));
+        menuContent = (Pane) loader.load();
+        menu.getChildren().add(menuContent);*/
+       menu.toFront();
+
+    }
+
+
+    private void SlideIn(Node s) {
+        TranslateTransition tt = new TranslateTransition(new Duration(500), s);
+        tt.setToX(0);
+        tt.play();
+    }
+
+    private void SlideOut(Node s) {
+        TranslateTransition tt = new TranslateTransition(new Duration(500), s);
+        tt.setToX(-725);
+        tt.play();
+    }
+
+    @FXML
+    public void HandleMenuButton(ActionEvent actionEvent) throws IOException {
+        SlideIn(menu);
+        menu.toFront();
+    }
+
+    @FXML
+    public void HandleNewGame(ActionEvent actionEvent) throws IOException {
+        SlideOut(menu);
+        menu.toFront();
     }
 
 
