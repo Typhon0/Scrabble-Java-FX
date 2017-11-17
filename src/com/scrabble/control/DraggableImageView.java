8ce68eb34455d22e7324d362b836747d50bb77f7
Typhon <loic.sculier@hotmail.fr>
2017-11-17 15:53:26 +0100

    import fix

--- src/com/scrabble/control/DraggableImageView.java
+++ src/com/scrabble/control/DraggableImageView.java
@@ -7,6 +7,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 /**
  * Draggable image view
