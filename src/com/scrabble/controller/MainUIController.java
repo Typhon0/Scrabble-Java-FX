7889eb5bf256d6fc82dbfff7df2fd869414036b8
LenoirRemi <biaremi@hotmail.fr>
2017-12-01 13:10:41 +0100

    Update graphique poser une piece

--- src/com/scrabble/controller/MainUIController.java
+++ src/com/scrabble/controller/MainUIController.java
@@ -2,6 +2,7 @@ package com.scrabble.controller;
 
 import com.scrabble.MainApp;
 import com.scrabble.control.ImageButton;
+import com.scrabble.model.BonusCase;
 import com.scrabble.model.Case;
 import com.scrabble.model.Piece;
 import com.scrabble.util.Animations;
@@ -17,10 +18,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 import java.awt.event.MouseEvent;
@@ -100,8 +98,8 @@ public class MainUIController {
             @Override
             public void run() {
                 boardGrid = (GridPane) board.getChildren().get(0);
-                ObservableList<Node> list= boardGrid.getChildren();
-                for(Node n:list){
+                ObservableList<Node> list = boardGrid.getChildren();
+                for (Node n : list) {
                     n.setOnMouseClicked(onBoardClicked());
                 }
                 scoreJ1.textProperty().bind(mainApp.getScrabble().getJoueur(0).nbPointsProperty().asString());
@@ -280,18 +278,14 @@ public class MainUIController {
 
     }
 
-    public Button generateButtonFromLetter(char ch, double size) {
+    public Button generateButtonFromLetter(char ch) {
         //TODO
         Button btn = new Button();
         btn.getStyleClass().add("buttonLetter");
         if (ch == '?') {
-            //btn.setText("?");
             btn.setStyle("-fx-background-image: url('/com/scrabble/ressources/Piece/letter.png')");
-            //btn.setStyle("-fx-background-size: 20px");
         } else {
-            //btn.setText(String.valueOf(ch));
             btn.setStyle("-fx-background-image: url('/com/scrabble/ressources/Piece/letter_" + ch + ".png')");
-            //btn.setStyle("-fx-background-size: 20px");
         }
         btn.setMinHeight(40);
         btn.setMinWidth(40);
@@ -302,10 +296,8 @@ public class MainUIController {
         ArrayList<Piece> main = mainApp.getScrabble().getJoueur(0).getMain();
         ArrayList<Button> listePiece = new ArrayList<Button>();
         mainJoueur.getChildren().clear();
-        double size = board.getWidth();
-        size /= 10;
         for (Piece p : main) {
-            Button btn = generateButtonFromLetter(p.getLettre(), size);
+            Button btn = generateButtonFromLetter(p.getLettre());
             listePiece.add(btn);
         }
         int i = 0;
@@ -332,22 +324,22 @@ public class MainUIController {
         return new EventHandler<javafx.scene.input.MouseEvent>() {
             @Override
             public void handle(javafx.scene.input.MouseEvent event) {
-                if(event.getSource() instanceof StackPane && LetterWaiting!=null){
+                if (event.getSource() instanceof StackPane && LetterWaiting != null) {
                     StackPane sp = (StackPane) event.getSource();
                     sp.getChildren().clear();
-                    LetterWaiting.setOnMouseClicked(null);
-                    LetterWaiting.setMinSize(sp.getWidth(),sp.getHeight());
-                    LetterWaiting.setMaxSize(sp.getWidth(),sp.getHeight());
-                    //TODO graphic
-                    LetterWaiting.getStyle().replace("-fx-background-size:35px;","-fx-background-size:"+ 10 +"px;");
+                    LetterWaiting.setOnMouseClicked(onBoardClicked());
+                    LetterWaiting.setMinSize(sp.getWidth(), sp.getHeight());
+                    LetterWaiting.setMaxSize(sp.getWidth(), sp.getHeight());
+                    LetterWaiting.getStyleClass().removeAll("buttonLetter");
+                    LetterWaiting.getStyleClass().add("resizingPiece");
                     sp.getChildren().add(LetterWaiting);
                     StackPane.setAlignment(LetterWaiting, Pos.CENTER);
 
                     //model gestion
-                    int numeroLettreDansMain = Integer.parseInt(LetterWaiting.getId().replace("B",""));
-                    int numeroDeCase = Integer.parseInt(sp.getId().replace("S",""));
+                    int numeroLettreDansMain = Integer.parseInt(LetterWaiting.getId().replace("B", ""));
+                    int numeroDeCase = Integer.parseInt(sp.getId().replace("S", ""));
                     //put in board
-                    mainApp.getScrabble().getBoard()[numeroDeCase%15][numeroDeCase/15].setPiece(mainApp.getScrabble().getJoueur(0).getMain().get(numeroLettreDansMain));
+                    mainApp.getScrabble().getBoard()[numeroDeCase % 15][numeroDeCase / 15].setPiece(mainApp.getScrabble().getJoueur(0).getMain().get(numeroLettreDansMain));
                     //remove in hand
                     mainApp.getScrabble().getJoueur(0).getMain().remove(numeroLettreDansMain);
 
@@ -357,5 +349,4 @@ public class MainUIController {
             }
         };
     }
-
 }
